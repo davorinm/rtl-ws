608d6cf588ae6589e435c7b3a4ccdf5fbfea2c26
Davorin Madaric <davorinmadaric@Davorins-MBP.home>
2023-06-30 18:04:17 +0200

    Update

--- plutoExample/build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-arm-linux-gnueabihf-gcc -mfloat-abi=hard --sysroot=files/sysroot -std=gnu99 -g -o pluto_stream src/ad9361-iiostream.c -lpthread -liio -lm -Wall -Wextra --warn-unused-variable
---
+++ plutoExample/dockerEntrypoint.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+export PATH=$PATH:/project/files/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf/bin
+
+make all
