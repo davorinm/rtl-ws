4cb46945ff788d16d320b5415141e401ec865c70
Davorin Madaric <davorinmadaric@Davorins-MBP.home>
2023-07-02 01:15:20 +0200

    Removed config

--- wsdr-pluto/src/tools/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-#include <stdint.h>
-
-/* returns milliseconds since Jan. 1. 1970 */
-uint64_t timestamp();
-
-#endif
--- wsdr-pluto/src/web/http_handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef HTTP_HANDLER_H
-#define HTTP_HANDLER_H
-
-#include <libwebsockets.h>
-
-struct lws_protocols* get_http_protocol();
-
-#endif
