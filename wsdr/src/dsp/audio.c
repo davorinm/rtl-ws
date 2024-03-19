1520e77741441ab6a323ac72322ef70799ab1255
Davorin Madaric <davorinmadaric@Davorins-MBP.home>
2024-03-19 22:38:51 +0100

    Refactor 10 Working, clean

--- wsdr/src/dsp/audio.c
+++ wsdr/src/dsp/audio.c
@@ -5,25 +5,24 @@
 #include "audio.h"
 #include "resample.h"
 #include "../tools/helpers.h"
-#include "../tools/list.h"
-
-#define AUDIO_BUFFER_POOL 50
 
 static float *demod_buffer = NULL;
 static int demod_buffer_len = 0;
+
 static float *work_audio_buffer = NULL;
 static int work_audio_buffer_len = 0;
 
-static struct list *full_audio_buffers = NULL;
-static struct list *used_audio_buffers = NULL;
+static float *play_audio_buffer = NULL;
+static int play_audio_buffer_len = 0;
 
+static float *audio_buffer = NULL;
 static int audio_buffer_len = 0;
+static int audio_buffer_count = 0;
+
 static pthread_mutex_t audio_mutex;
 
 void audio_init()
 {
-    used_audio_buffers = list_alloc();
-    full_audio_buffers = list_alloc();
     pthread_mutex_init(&audio_mutex, NULL);
 }
 
@@ -31,14 +30,7 @@ void audio_reset()
 {
     pthread_mutex_lock(&audio_mutex);
 
-    list_apply(full_audio_buffers, free);
-    list_apply(used_audio_buffers, free);
-    list_clear(full_audio_buffers);
-    list_clear(used_audio_buffers);
-
     demod_buffer_len = 0;
-    work_audio_buffer_len = 0;
-    audio_buffer_len = 0;
 
     pthread_mutex_unlock(&audio_mutex);
 }
@@ -56,25 +48,20 @@ void audio_fm_demodulator(const cmplx_dbl *signal, int len)
     {
         DEBUG("Reallocating demodulation and audio buffers %d\n", len);
 
+        pthread_mutex_lock(&audio_mutex);
+
         demod_buffer_len = len;
         demod_buffer = (float *)realloc(demod_buffer, demod_buffer_len * sizeof(float));
 
         work_audio_buffer_len = len / 2;
         work_audio_buffer = (float *)realloc(work_audio_buffer, work_audio_buffer_len * sizeof(float));
 
-        pthread_mutex_lock(&audio_mutex);
+        play_audio_buffer_len = work_audio_buffer_len / 2;
+        play_audio_buffer = (float *)realloc(play_audio_buffer, play_audio_buffer_len * sizeof(float));
 
-        list_apply(full_audio_buffers, free);
-        list_apply(used_audio_buffers, free);
-        list_clear(full_audio_buffers);
-        list_clear(used_audio_buffers);
-
-        audio_buffer_len = work_audio_buffer_len / 2;
-
-        for (i = 0; i < AUDIO_BUFFER_POOL; i++)
-        {
-            list_add(used_audio_buffers, calloc(audio_buffer_len, sizeof(float)));
-        }
+        audio_buffer_len = len * 2;
+        audio_buffer = (float *)realloc(audio_buffer, audio_buffer_len * sizeof(float));
+        audio_buffer_count = 0;
 
         pthread_mutex_unlock(&audio_mutex);
     }
@@ -106,56 +93,42 @@ void audio_fm_demodulator(const cmplx_dbl *signal, int len)
 
     pthread_mutex_lock(&audio_mutex);
 
-    if (list_length(used_audio_buffers) > 0)
+    halfband_decimate(work_audio_buffer, play_audio_buffer, play_audio_buffer_len, delay_line_2);
+
+    // Check space
+    if (audio_buffer_len - audio_buffer_count < play_audio_buffer_len)
     {
-        halfband_decimate(work_audio_buffer, list_peek(used_audio_buffers), audio_buffer_len, delay_line_2);
-        list_poll_to_list(used_audio_buffers, full_audio_buffers);
+        // Make space by shifting
+        memcpy(audio_buffer, audio_buffer + play_audio_buffer_len, (audio_buffer_count - play_audio_buffer_len) * sizeof(float));
     }
+
+    // Move to audio
+    memcpy(audio_buffer + audio_buffer_count, play_audio_buffer, play_audio_buffer_len * sizeof(float));
+    audio_buffer_count += play_audio_buffer_len;
 
     pthread_mutex_unlock(&audio_mutex);
 }
 
 int audio_new_audio_available()
 {
-    if (full_audio_buffers == NULL)
-    {
-        return 0;
-    }
-
-    return list_length(full_audio_buffers) > 0;
+    return audio_buffer_count > 0;
 }
 
 int audio_get_audio_payload(char *buf, int buf_len)
 {
-    static int audio_buf_cur_idx = 0;
-    float *data = NULL;
-    int new_samples = 0;
-    int copied_samples = 0;
-    int buf_samples_remaining = buf_len / sizeof(float);
+    pthread_mutex_lock(&audio_mutex);
 
-    do
-    {
-        pthread_mutex_lock(&audio_mutex);
-        data = list_peek(full_audio_buffers);
-        if (data != NULL)
-        {
-            if (audio_buf_cur_idx >= audio_buffer_len)
-            {
-                list_poll_to_list(full_audio_buffers, used_audio_buffers);
-                audio_buf_cur_idx = 0;
-            }
-            new_samples = audio_buffer_len - audio_buf_cur_idx;
-            new_samples = (new_samples <= buf_samples_remaining) ? new_samples : buf_samples_remaining;
-            if (new_samples > 0)
-            {
-                memcpy(&(buf[copied_samples]), &(data[audio_buf_cur_idx]), new_samples * sizeof(float));
-                audio_buf_cur_idx += new_samples;
-                copied_samples += new_samples;
-                buf_samples_remaining -= new_samples;
-            }
-        }
-        pthread_mutex_unlock(&audio_mutex);
-    } while (data != NULL && buf_samples_remaining > 0);
+    int buf_len_samples = buf_len / sizeof(float);
+    int copied_samples = MIN(buf_len_samples - 2, audio_buffer_count);
+
+    // Copy to output buffer
+    memcpy(buf, audio_buffer, copied_samples * sizeof(float));
+    audio_buffer_count -= copied_samples;
+
+    // Move used data
+    memcpy(audio_buffer, audio_buffer + copied_samples, audio_buffer_count * sizeof(float));
+
+    pthread_mutex_unlock(&audio_mutex);
 
     return copied_samples * sizeof(float);
 }
@@ -166,10 +139,6 @@ void audio_close()
 
     free(demod_buffer);
     free(work_audio_buffer);
-
-    list_apply(used_audio_buffers, free);
-    list_clear(used_audio_buffers);
-
-    list_apply(full_audio_buffers, free);
-    list_clear(full_audio_buffers);
+    free(play_audio_buffer);
+    free(audio_buffer);
 }
